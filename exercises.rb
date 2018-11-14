411b4a41813203a81955f1417458b6e349ed7ca0
davepodgorski <davepodgorski@gmail.com>
2018-11-14 15:03:12 -0500

    0 done

---
+++ exercises.rb
@@ -0,0 +1,23 @@
+fav_colours = ["orange", "blue", "red", "green"]
+ages = [32, 30, 22, 29, 28, 32, 30,]
+coin_flips = ["heads", "heads", "heads", "tails", "heads"]
+fav_bands = ["The Eagles", "The Temptations", "The Supremes"]
+fav_colours = [:orange, :blue, :red, :green]
+
+
+dictionary =
+{homunculus: "a tiny man", dog: "a small domesticated animal", unicorn: "a mythical horse with a horn on its head"}
+fav_movies =
+{The_Godfather: 1977, The_Sting: 1976, The_Right_Stuff: 1973}
+
+cities =
+{Toronto: 3000000, New_York: 12000000, Chicago: 3000000}
+
+friends =
+{James: 28, Alina: 25, Rezzie: 33}
+
+puts coin_flips
+puts fav_colours[0]
+puts ages.sort
+ages << 0
+puts fav_movies[:The_Sting]
