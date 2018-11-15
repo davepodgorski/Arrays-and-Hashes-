759b7644bb0d383bd267fba2d7dd1eaf44ef3a49
davepodgorski <davepodgorski@gmail.com>
2018-11-14 19:29:10 -0500

    victory!

---
+++ exercises6andmore.rb
@@ -0,0 +1,36 @@
+movies  = {
+  1999 => ["The_Matrix", "Star_Wars:_Episode_1", "The_Mummy"],
+  2009 => ["Avatar", "Star_Trek", "District_9"],
+  2019 => ["How_to_Train_Your_Dragon_3", "Toy_Story_4", "Star_Wars_Episode_9"]
+  }
+
+
+phone_buttons = [[1, 2, 3 ], [4, 5, 6], [7, 8, 9], ["*", 0, "#"]]
+
+countries = [Germany = {continent: "Europe", island: "no"},
+Canada = {continent: "America", island: "no"},
+Japan = {continent: "Oceania", island: "yes"}]
+
+# 20.times do
+#   puts "I will not skateboard in the halls."
+# end
+#
+# lines = []
+# 20.times do |statement|
+#  lines.push ("I will not skateboard in the halls. #{statement}")
+#  end
+# puts lines
+
+# numbers  = [*1..50]
+# total = 0
+# numbers.each do |number|
+#   total = total + number
+# end
+#
+# numbers = [*1..50]
+# triple = []
+# numbers.each do |repeat|
+#   3.times do
+#     triple.push(repeat)
+# end
+# end
